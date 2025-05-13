bd48adb40044e54c1cd666afce1a3e7fedf8f3d0
Shaun Kassa <shaun.lulu.kassa@gmail.com>
2025-05-13 11:21:54 -0700

    Fixed styling and layout

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#0a0a0a",
+        offWhite: "#f4f4f4",
+        accentRed: "#b22222",
+        accentGold: "#d4af37",
+      },
+      fontFamily: {
+        sans: ["Inter", "Poppins", "sans-serif"],
+        serif: ["Playfair Display", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
